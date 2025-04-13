1f41632e1d4a1fd2394e250c4ca9f6b474d23688
luka chikashvili <lukachikashvili2020@gmail.com>
2025-04-13 23:40:54 +0400

    add clerck sign in and sign out features

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 
-const Header = () => {
+import React from 'react'
+import { Button } from './ui/button'
+import { LogIn } from 'lucide-react'
+
+const Header = async ({ isAdminPage = false}) => {
+    
   return (
     <div>
         <div>
             logo
         </div>
 
-        
+        <div>
+            <SignedIn>
+              <h1>you are signed in</h1>
+              
+            </SignedIn>
+
+            <SignedOut>
+                <SignInButton forceRedirectUrl='/'>
+                  <Button><LogIn /> sign in</Button>
+                </SignInButton>
+              
+            </SignedOut>
+
+            <SignedIn>
+            <UserButton appearance={
+              {
+                elements: {
+                 avatarImage: "w-36 h-36"
+                }
+              }
+            } />
+          </SignedIn>
+
+
+        </div>
       </div>
   )
 }
