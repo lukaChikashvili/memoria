d12449f34e262374b8bdfac9cabd98337bbaec67
luka chikashvili <lukachikashvili2020@gmail.com>
2025-04-22 20:51:19 +0400

    add screenshot to db server action

--- client/src/actions/memorials.js
+++ client/src/actions/memorials.js
@@ -6,7 +6,6 @@ import { db } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 import { cookies } from "next/headers";
-import { userAgent } from "next/server";
 import { v4 as uuidv4 } from "uuid";
 
 async function fileToBase64(file) {
@@ -138,4 +137,73 @@ async function fileToBase64(file) {
     });
   
     return user?.cloth?.[0] ?? null;
+  }
+
+
+
+  export async function addScreenShots(images) {
+
+    const { userId } = await auth();
+    if(!userId) throw new Error("Unauthorized");
+
+    const user = await db.user.findUnique({
+        where: {
+            clerkUserId: userId
+        }
+    });
+
+    if(!user) throw new Error("User not found");
+
+    try {
+
+      const memorialId = uuidv4();
+      const folderPath = `memorials/${memorialId}`;
+
+      const cookieStore = await cookies();
+      const supabase = createClient(cookieStore);
+
+      const imageUrls = [];
+
+    for (let i = 0; i < images.length; i++) {
+       const base64Data = images[i];
+
+    
+    if (!base64Data || !base64Data.startsWith("data:image/")) {
+      console.warn("Skipping invalid image data");
+      continue;
+    }
+  }
+
+  const base64 = base64Data.split(",")[1];
+    const imageBuffer = Buffer.from(base64, "base64");
+
+    const mimeMatch = base64Data.match(/data:image\/([a-zA-Z0-9]+);/);
+    const fileExtension = mimeMatch ? mimeMatch[1] : "jpeg";
+
+    const fileName = `image-${Date.now()}-${i}.${fileExtension}`;
+    const filePath = `${folderPath}/${fileName}`;
+
+    const { data, error } = await supabase.storage.
+    from("memoria-images").upload(filePath, imageBuffer, {
+      contentType: `image/${fileExtension}`,
+    });
+
+    if (error) {
+      console.error("Error uploading image:", error);
+      throw new Error(`Failed to upload image: ${error.message}`);
+    }
+
+    const publicUrl = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/memoria-images/${filePath}`;
+
+    imageUrls.push(publicUrl);
+
+    if (imageUrls.length === 0) {
+      throw new Error("No valid images were uploaded");
+    }
+
+
+      
+    } catch (error) {
+      
+    }
   }
